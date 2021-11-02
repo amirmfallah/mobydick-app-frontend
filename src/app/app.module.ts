f0d1a6511824b55fc1a60740708ff05c4e68bab2
Amir Mohammad Fallah <amirmfallah@yahoo.com>
2021-11-02 15:02:43 +0330

    [add] home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UiKitModule } from './ui-kit/ui-kit.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, UiKitModule],
   providers: [],
   bootstrap: [AppComponent],
