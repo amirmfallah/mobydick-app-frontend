e2c80ee48720dbf16200644a71ae067248356159
siavashbis <siavashbis@gmail.com>
2021-11-06 20:48:44 +0330

    [add] search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HomeComponent } from './home/home.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { SliderComponent } from './categories/slider/slider.component';
 import { SliderItemComponent } from './categories/slider/slider-item/slider-item.component';
+import { SearchComponent } from './uk-kit/search/search.component';
 
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, CategoriesComponent, SliderComponent, SliderItemComponent],
+  declarations: [AppComponent, HomeComponent, CategoriesComponent, SliderComponent, SliderItemComponent, SearchComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, UiKitModule],
   providers: [],
   bootstrap: [AppComponent],
