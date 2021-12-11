d7447b10cfa2cc258bbd5e0e8d0c8b4cf3ab4148
siavashbis <siavashbis@gmail.com>
2021-12-11 17:24:56 +0330

    add productbyid service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GetBranchByIdService } from './services/get-branch-by-id.service';
+import { ActivatedRoute } from '@angular/router';
+import { branchbyid } from './interfaces/branchbyid.interface';
+import { ProductComponent } from '../product/product.component';
+import { Product } from 'src/core/interfaces/product.interface';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  carouselimages = [];
+  favouriteProducts: Product[]
+  branch: branchbyid;
+  constructor(
+    private branchService: GetBranchByIdService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id="61b359d03de833e1093b3786";
+    this.branchService.getBranch(id).subscribe((res: branchbyid) => {
+      this.branch = res;
+      this.carouselimages=res.sliderPictures;
+      this.favouriteProducts=res.favoriteProducts;
+      console.log(this.favouriteProducts)
+    });
   }
-
 }
+
+
---
+++ src/app/home/interfaces/branchbyid.interface.ts
@@ -0,0 +1,12 @@
+import { Product } from 'src/core/interfaces/product.interface';
+
+export interface branchbyid {
+  sliderPictures: string[];
+  favoriteProducts: Product[];
+  _id: string;
+  name: string;
+  thumbnail: string;
+  description: string;
+}
+
+
