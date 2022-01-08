1f5be6f8bb25b593b4e2842ecb6091e4231f75e1
Amir Mohammad Fallah <amirmfallah@yahoo.com>
2022-01-08 14:45:42 +0400

    [add] auth page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,19 @@
+import { AuthService } from 'src/core/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/', 'sign-in']);
   }
-
 }
--- src/app/ui-kit/code-sent/code-sent.component.ts
+++ src/app/ui-kit/code-sent/code-sent.component.ts
@@ -1,3 +1,4 @@
+import { BehaviorSubject } from 'rxjs';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
@@ -9,8 +10,13 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 export class CodeSentComponent implements OnInit {
   @Input() form: FormGroup;
   @Output() formSubmit: EventEmitter<void> = new EventEmitter();
+  @Input() codeSent: BehaviorSubject<boolean>;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  back() {
+    this.codeSent.next(false);
+  }
 }
